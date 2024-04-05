f9dae094bf54a7412557028b5f1212cad779860a
unfreeshooter_git <89206050570@mail.ru>
2024-04-05 23:47:32 +0300

    get_text

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,13 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
     @Test
     public void testHelloWorld(){
-
-        System.out.println("Hello world");
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 }
